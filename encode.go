4f2bc95bc1d5fbed4c69c2419f66e3236b6f8fa7
Pascal Vorwerk <info@fossores.de>
2017-06-24 16:47:38 +0200

    Add a readme and switch UUID implementation
    Add a general description to the readme and switch to customized UUID implementation

--- encode.go
+++ encode.go
@@ -1,12 +1,13 @@
 package jwt
 
 import (
-	"strings"
-	"time"
 	"encoding/base64"
 	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/fossoreslp/go.uuid"
 	"golang.org/x/crypto/ed25519"
-	"github.com/satori/go.uuid"
 )
 
 func Encode(content JWTContent) (token string) {
@@ -16,20 +17,20 @@ func Encode(content JWTContent) (token string) {
 	content.Iat = time.Now().Unix()
 	content.Jti = uuid.NewV4().String()
 	content.Aud = "ChatClient"
-	content.Exp = time.Now().Add(time.Duration(604800)*time.Second).Unix()
+	content.Exp = time.Now().Add(time.Duration(604800) * time.Second).Unix()
 	content.Nbf = time.Now().Unix()
 	data.Content = content
 	header, err := json.Marshal(&data.Header)
 	var b64header string
-	if (err == nil) {
+	if err == nil {
 		b64header = base64.URLEncoding.EncodeToString(header)
 	}
 	jsonContent, err := json.Marshal(&data.Content)
 	var b64content string
-	if (err == nil) {
+	if err == nil {
 		b64content = base64.URLEncoding.EncodeToString(jsonContent)
 	}
-	if (b64header != "" && b64content != "") {
+	if b64header != "" && b64content != "" {
 		tokenData := strings.Join([]string{b64header, b64content}, ".")
 		hash := ed25519.Sign(keys.PrivateKey, []byte(tokenData))
 		b64hash := base64.URLEncoding.EncodeToString(hash)
