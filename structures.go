46e839ffd8e0192053db05ee745a54b35dd900f0
Pascal Vorwerk <info@fossores.de>
2017-06-24 18:05:30 +0200

    Use lowercase names in JSON
    Use lowercase names for the elements in JSON to comply with the JWT standard

--- structures.go
+++ structures.go
@@ -2,19 +2,19 @@ package jwt
 
 // Header contains the header data of a JSON web token
 type Header struct {
-	Typ string
-	Alg string
+	Typ string `json:"typ"`
+	Alg string `json:"alg"`
 }
 
 // Content contains the main content of a JSON web token
 type Content struct {
-	Iss string
-	Sub string
-	Aud string
-	Exp int64
-	Nbf int64
-	Iat int64
-	Jti string
+	Iss string `json:"iss"`
+	Sub string `json:"sub"`
+	Aud string `json:"aud"`
+	Exp int64  `json:"exp"`
+	Nbf int64  `json:"nbf"`
+	Iat int64  `json:"iat"`
+	Jti string `json:"jti"`
 }
 
 // JWT contains the header and content of a JSON web token as well booleans indicating the validity of the token
